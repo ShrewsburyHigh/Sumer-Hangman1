8e7484d81b8a6f240f54f2b54ef2ac1a71e7f840
tcalabresi <30498811+tcalabresi@users.noreply.github.com>
2020-02-25 09:47:34 -0500

    Update Hangman_Sumer_Shinde.py

--- Hangman_Sumer_Shinde.py
+++ Hangman_Sumer_Shinde.py
@@ -1,10 +1,11 @@
+# test2TC
 secret = ""
 letter = ""
 def test():
     if (letter in secret):
         pos = secret.find(letter)
         updatedWord[pos] = letter
-        print updatedWord.join()
+        print( updatedWord.join())
         ifWon()
     else:
         getLetter()
